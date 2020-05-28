22a1b53cd59542fe56ad5fab68af1a7ae0a1d8a1
SaraCremonesi <saracrem00@gmail.com>
2020-05-28 18:06:19 +0200

    aggiornamenti

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+// ***************JSNACK 1*******************
+// var firstNumber = parseInt(prompt('Inserisci un numero'));
+// var secondNumber = parseInt(prompt('Inserisci un numero'));
+//
+// while (firstNumber != '' && secondNumber != '') {
+//   if (firstNumber > secondNumber) {
+//     alert(firstNumber);
+//   } else if (firstNumber < secondNumber) {
+//     alert(secondNumber);
+//   } else {
+//     alert('i numeri inseriti sono uguali');
+//   };
+// };
+
+// ***************JSNACK 2*******************
+ var firstWord = prompt('Inserisci una parola');
+ var secondWord = prompt('Inserisci una parola');
+
+ while (isNaN(firstWord) && firstWord != '' && isNaN(secondWord) && secondWord != '') {
+   if (firstWord.length < secondWord.length) {
+     alert(firstWord);
+     alert(secondWord);
+   } else if (firstWord.length > secondWord.length) {
+     alert(secondWord);
+     alert(firstWord);
+   } else {
+     alert('Le parole inserte hanno la stessa lunghezza');
+   }
+ };
 
 // ***************JSNACK 4*******************
 // PRIMO MODO
@@ -92,5 +121,3 @@
 // };
 //
 // alert(somma);
-
-// *************** *******************
