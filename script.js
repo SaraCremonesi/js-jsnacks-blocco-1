928e7f757ad2d2c873f66c6a8d8f72b9a81b760f
SaraCremonesi <saracrem00@gmail.com>
2020-06-05 18:06:48 +0200

    aggiornamento nuovi jsnacks

--- script.js
+++ script.js
@@ -138,3 +138,66 @@
 // };
 //
 // alert(somma);
+
+// ***************JSNACK 6*******************
+var n = parseInt(prompt('Inserisci un numero'));
+console.log(n);
+for (var i = 0; i < n; i++) {
+  var array = [];
+  for (var j = 0; j < 10; j++) {
+    var numeroCasuale = Math.floor(Math.random() * 100 + 1);
+    array.push(numeroCasuale);
+  }
+  console.log(array);
+};
+
+//***************JSNACK 7*******************
+// var arrayNomiInvitati = ['Joe', 'Abramo', 'Pinco'];
+// var arrayCognomiInvitati = ['Black', 'Lincoln', 'Pallo'];
+// var listaIdentitaCasuali = [];
+//
+// for (var i = 0; i < arrayNomiInvitati.length; i++) {
+//   var indiceNomeCasuale = Math.floor(Math.random() * arrayNomiInvitati.length);
+//   var nomeCasuale = arrayNomiInvitati[indiceNomeCasuale];
+//
+//   var indiceCognomeCasuale = Math.floor(Math.random() * arrayCognomiInvitati.length);
+//   var cognomeCasuale = arrayCognomiInvitati[indiceCognomeCasuale];
+//
+//   var identitaCasuale = nomeCasuale + ' ' + cognomeCasuale;
+//
+//   listaIdentitaCasuali.push(identitaCasuale);
+// };
+//
+// console.log(listaIdentitaCasuali);
+
+//***************JSNACK 8*******************
+// var arrayUno = [1, 2, 3, 4, 5];
+// var arrayDue = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+// console.log('Primo array iniziale: ', arrayUno);
+// console.log('Secondo array iniziale: ', arrayDue);
+//
+// while (arrayUno.length != arrayDue.length) {
+//   var numeroCasuale = Math.floor(Math.random() * arrayDue.length);
+//   arrayUno.push(numeroCasuale);
+// };
+// console.log('Primo array finale: ', arrayUno);
+// console.log('Secondo array finale: ', arrayDue);
+
+//***************JSNACK 9*******************
+// var odd = document.getElementById('red-odd');
+// var even = document.getElementById('green-even');
+//
+// var arrayNumeri = [];
+// for (var i = 0; i < 10; i++) {
+//   var numeroCasuale = Math.floor(Math.random() * 10 + 1);
+//   arrayNumeri.push(numeroCasuale);
+// };
+// console.log(arrayNumeri);
+//
+// for (var i = 0; i < arrayNumeri.length; i++) {
+//   if (arrayNumeri[i] % 2 === 0) {
+//     even.innerHTML += arrayNumeri[i] + '<br>'
+//   } else {
+//     odd.innerHTML += arrayNumeri[i] + '<br>'
+//   };
+// };
